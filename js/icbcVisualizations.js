fe227873a9f84327e7afc7233a12fef7ae38d5b8
irina-ng <rinaray2004@gmail.com>
2026-03-30 19:07:08 -0700

    Visualization for iCBC heatmap

---
+++ js/icbcVisualizations.js
@@ -0,0 +1,50 @@
+console.log("vis.js is connected");
+
+async function fetchData() {
+  const data = await d3.csv("./data/ICBC_CrashesMonths.csv", d3.autoType);
+  return data;
+}
+
+async function render(viewID, spec) {
+  const result = await vegaEmbed(viewID, spec);
+  result.view.run();
+}
+
+fetchData().then(async (data) => {
+  const icbc = vl
+    .markRect()
+    .data(data)
+    .encode(
+      vl.y().fieldN("Year").title("Year"),
+      vl.x()
+        .fieldN("Month")
+        .title("Month"),
+
+      vl.color()
+        .fieldQ("Unrounded Count")
+        .title("Crashes")
+        .scale({
+          scheme: "yelloworangered",
+          domain: [0, 3500]
+        }),
+      vl.tooltip([
+        vl.fieldN("Month"),
+        vl.fieldN("Year"),
+        vl.fieldQ("Unrounded Count")
+      ])
+    )
+    .title({
+      text: "Crashes between 2020–2025 by months"
+    })
+    .height(400)
+    .width(700)
+    .toSpec();
+
+  render(".icbc-vis", icbc);
+
+});
+
+async function render(viewID, spec) {
+  const result = await vegaEmbed(viewID, spec);
+  result.view.run();
+}
